14318cd57cfb156b723ab19252db5fdb59832318
Pavel Ryapolov <paulustg@gmail.com>
2021-10-09 18:30:10 +0400

    Обновлен линтер. Рефакторинг функций создания объекта объявлений. Оптимизированы функции random. Удалены лишние файлы.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,30 @@
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME_OF_CHECK = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomNumber = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  min = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  max = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
 
-  if (min >= 0 && max >= 0) {
-    if (max < min) {
-      const swap = max;
-      max = min;
-      min = swap;
-    }
-    if (max > min) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return min;
-  }
-  return 'Используйте диапазон положительных чисел';
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-const getRandomFloat = (min, max, numAfterPoint) => {
-  if (min >= 0 && max >= 0) {
-    if (max < min) {
-      const swap = max;
-      max = min;
-      min = swap;
-    }
-    if (max > min) {
-      return +((Math.random() * (max - min)) + min).toFixed(numAfterPoint);
-    }
-    return min;
-  }
-  return 'Используйте диапазон положительных чисел';
+const getRandomFloat = (min, max, numAfterPoint = 1) => {
+  min = Math.min(Math.abs(min), Math.abs(max));
+  max = Math.max(Math.abs(min), Math.abs(max));
+
+  return +((Math.random() * (max - min)) + min).toFixed(numAfterPoint);
 };
 
 // Генерация url аватара автора
-const getAuthorUrl = () => {
-  let url = '0';
-  const randomNumber = getRandomNumber(0, 9);
-  randomNumber === 0 ? url = '10' : url += randomNumber;
-  return 'img/avatars/user' + url + '.png';
+const getAuthorUrl = (id) => {
+  const userID = `0${id}`.slice(-2);
+  return `img/avatars/user${userID}.png`;
 };
 
 // Получаем массив строк случайной длины из массива
@@ -54,17 +40,10 @@ const getRandomRangeFromArray = (arrayToRange) => {
   return arr;
 };
 
-// Создание массива из объектов
-const getAd = (title, description) => {
-  const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const TIME_OF_CHECK = ['12:00', '13:00', '14:00'];
-  const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-
+// Создание объекта для описания похожих объявлений
+const getAd = (userID, title, description) => {
   const author = {
-    avatar: getAuthorUrl(),
+    avatar: getAuthorUrl(userID),
   };
 
   const location = {
@@ -74,7 +53,7 @@ const getAd = (title, description) => {
 
   const offer = {
     title: title,
-    address: '' + location.lat + ', ' + location.lng,
+    address: `${location.lat}, ${location.lng}`,
     price: getRandomNumber(1, 100) * 1000,
     type: TYPES[getRandomNumber(0, TYPES.length - 1)],
     rooms: getRandomNumber(1, 3),
@@ -86,11 +65,15 @@ const getAd = (title, description) => {
     photos: getRandomRangeFromArray(PHOTOS),
   };
 
-  return [author, offer, location];
+  return {author, offer, location};
 };
 
-const arrayOfAds = [];
-for (let i = 1; i <= 10; i++) {
-  arrayOfAds.push(getAd('Сдаётся жильё', 'Сдам жильё. Недорого.'));
-}
-console.log(arrayOfAds);
+// Создание массива из 10-ти объектов
+const createAds = () => {
+  const arrayOfAds = [];
+  for (let i = 1; i <= 10; i++) {
+    arrayOfAds.push(getAd(i, 'Сдаётся жильё', 'Сдам жильё. Недорого.'));
+  }
+  return arrayOfAds;
+};
+createAds();

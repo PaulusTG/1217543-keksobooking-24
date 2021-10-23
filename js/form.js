80ac37d352e61962344a3643e0428cd48eeafc91
Pavel Ryapolov <paulustg@gmail.com>
2021-10-23 18:43:58 +0400

    Оптимизирована функция переключения режима формы. Добавлен параметр-переключатель

--- js/form.js
+++ js/form.js
@@ -1,35 +1,20 @@
 const adForm = document.querySelector('.ad-form');
-const formFieldsets = adForm.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
-const mapChildrens = mapFilters.querySelectorAll('.map__filter');
-const mapFieldset = mapFilters.querySelector('fieldset');
+const activeElements = document.querySelectorAll('.ad-form fieldset, .map__filter, .map__filters fieldset');
 
-const disableForm = () => {
-  adForm.classList.add('ad-form--disabled');
+// Переключение режима формы и фильтров (активно/неактивно)
+const changeFormState = (isDisabled = true) => {
+  if (isDisabled) {
+    adForm.classList.add('ad-form--disabled');
+    mapFilters.classList.add('map__filters--disabled');
+  } else {
+    adForm.classList.remove('ad-form--disabled');
+    mapFilters.classList.remove('map__filters--disabled');
+  }
 
-  formFieldsets.forEach((fieldset) => {
-    fieldset.disabled = true;
-  });
-
-  mapFilters.classList.add('map__filters--disabled');
-  mapFieldset.disabled = true;
-  mapChildrens.forEach((mapChildren) => {
-    mapChildren.disabled = true;
-  });
-};
-
-const enableForm = () => {
-  adForm.classList.remove('ad-form--disabled');
-
-  formFieldsets.forEach((fieldset) => {
-    fieldset.disabled = false;
-  });
-
-  mapFilters.classList.remove('map__filters--disabled');
-  mapFieldset.disabled = false;
-  mapChildrens.forEach((mapChildren) => {
-    mapChildren.disabled = false;
+  activeElements.forEach((activeElement) => {
+    activeElement.disabled = isDisabled;
   });
 };
 
-export {disableForm, enableForm};
+export {changeFormState};
